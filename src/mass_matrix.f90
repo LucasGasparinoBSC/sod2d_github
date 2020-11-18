20343f3ea2561546c9cab7b248384254a39ceb9f
lgaspari <lucas.gasparino@bsc.es>
2020-11-18 16:51:38 +0100

    Finished subroutines to compute consistent and lumped mass matrices

--- src/mass_matrix.f90
+++ src/mass_matrix.f90
@@ -2,10 +2,20 @@ module mass_matrix
 
       contains
 
-              subroutine consistent_mass()
+              subroutine consistent_mass(nelem,nnode,npoin,ngaus,connec,gpvol,Ngp,Mc)
+
                       implicit none
+
+                      integer(4), intent(in)  :: nelem, nnode, npoin, ngaus
+                      integer(4), intent(in)  :: connec(nelem,nnode)
+                      real(8),    intent(in)  :: gpvol(1,ngaus,nelem), Ngp(ngaus,nnode)
+                      real(8),    intent(out) :: Mc(npoin,npoin)
+                      integer(4)              :: ielem, igaus, inode, jnode, ind(nnode)
+                      real(8)                 :: Me(nnode,nnode)
+
                       Mc = 0.0d0
                       do ielem = 1,nelem
+                         ind(1:nnode) = connec(ielem,1:nnode)
                          Me = 0.0d0
                          do igaus = 1,ngaus
                             do inode = 1,nnode
@@ -15,10 +25,41 @@ module mass_matrix
                                end do
                             end do
                          end do
+                         if (ielem == 1) then
+                            print*, Me(1,:)
+                         end if
+                         Mc(ind,ind) = Mc(ind,ind)+Me
                       end do
+
               end subroutine consistent_mass
 
-              subroutine lumped_mass()
+              subroutine lumped_mass(nelem,nnode,npoin,ngaus,connec,gpvol,Ngp,Ml)
+
+                      implicit none
+
+                      integer(4), intent(in)  :: nelem, nnode, npoin, ngaus
+                      integer(4), intent(in)  :: connec(nelem,nnode)
+                      real(8),    intent(in)  :: gpvol(1,ngaus,nelem), Ngp(ngaus,nnode)
+                      real(8),    intent(out) :: Ml(npoin)
+                      integer(4)              :: ielem, igaus, inode, jnode, ind(nnode)
+                      real(8)                 :: Me(nnode)
+
+                      Ml = 0.0d0
+                      do ielem = 1,nelem
+                         ind(1:nnode) = connec(ielem,1:nnode)
+                         Me = 0.0d0
+                         do igaus = 1,ngaus
+                            do inode = 1,nnode
+                               do jnode = 1,nnode
+                                  Me(inode) = Me(inode) + &
+                                     (gpvol(1,igaus,ielem)/(Ngp(igaus,jnode)**2)) * &
+                                     (Ngp(igaus,inode)**2)
+                               end do
+                            end do
+                         end do
+                         Ml(ind) = Ml(ind)+Me
+                      end do
+
               end subroutine lumped_mass
 
 end module mass_matrix
