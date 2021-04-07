6fc6e8987946b151db39f40c945a84724000786b
lucas gasparino <lucas.gasparino@bsc.es>
2021-04-07 17:04:10 +0200

    Momentum unit test

---
+++ unitt/unitt_mom_convec/unitt_mom_convec.f90
@@ -0,0 +1,169 @@
+program unitt_mom_convec
+
+        !*********************************************************************!
+        ! Unit test for generic scalar convection subroutine. Uses hard-coded !
+        ! information to be independent from other subroutines.               !
+        !*********************************************************************!
+
+        use elem_convec
+
+        implicit none
+
+        integer(4)                 :: ndime, nnode, ngaus
+        integer(4)                 :: idime, inode, igaus
+        integer(4)                 :: nelem, npoin
+        integer(4)                 :: ielem, ipoin
+        integer(4), allocatable    :: connec(:,:)
+        real(8),    allocatable    :: coord(:,:), elcod(:,:)
+        real(8),    allocatable    :: xgp(:,:), wgp(:)
+        real(8),    allocatable    :: N(:), dN(:,:), Ngp(:,:), dNgp(:,:,:)
+        real(8),    allocatable    :: Je(:,:), He(:,:)
+        real(8),    allocatable    :: struc_J(:,:,:,:), struc_H(:,:,:,:), struc_detJ(:,:,:)
+        real(8),    allocatable    :: dxN(:,:), gpcar(:,:,:,:), gpvol(:,:,:)
+        real(8),    allocatable    :: u(:,:), q(:,:), pr(:), Rconvec(:,:)
+        real(8)                    :: s, t, detJe
+
+        !*********************************************************************!
+        ! Basic data                                                          !
+        !*********************************************************************!
+
+        ndime = 2
+        nnode = 4
+        nelem = 2
+        npoin = 6
+        ngaus = 4
+
+        allocate(connec(nelem,nnode))
+        allocate(coord(npoin,ndime))
+
+        connec(1,:) = (/1,2,5,6/)
+        connec(2,:) = (/2,3,4,5/)
+
+        coord(1,:) = (/0.0d0, 0.0d0/)
+        coord(2,:) = (/2.0d0, 0.0d0/)
+        coord(3,:) = (/3.0d0, 0.0d0/)
+        coord(4,:) = (/3.5d0, 3.0d0/)
+        coord(5,:) = (/2.0d0, 2.0d0/)
+        coord(6,:) = (/0.0d0, 2.0d0/)
+
+        !*********************************************************************!
+        ! Allocate variables                                                  !
+        !*********************************************************************!
+
+        allocate(u(npoin,ndime))  ! velocity
+        allocate(q(npoin,ndime))  ! Momentum
+        allocate(pr(npoin))       ! Pressure
+        u(1,:) = (/1.0, 1.0/)
+        u(2,:) = (/1.5, 1.5/)
+        u(3,:) = (/1.0, 1.0/)
+        u(4,:) = (/1.0, 1.0/)
+        u(5,:) = (/1.5, 1.5/)
+        u(6,:) = (/1.0, 1.0/)
+        q = 1.20d0*u
+        pr = 0.5d0
+
+        !*********************************************************************!
+        ! Generate GLL table                                                  !
+        !*********************************************************************!
+
+        allocate(xgp(ngaus,ndime))
+        allocate(wgp(ngaus))
+
+        xgp(1,:) = (/-1.0d0/sqrt(3.0d0), -1.0d0/sqrt(3.0d0)/)
+        xgp(2,:) = (/1.0d0/sqrt(3.0d0), -1.0d0/sqrt(3.0d0)/)
+        xgp(3,:) = (/1.0d0/sqrt(3.0d0), 1.0d0/sqrt(3.0d0)/)
+        xgp(4,:) = (/-1.0d0/sqrt(3.0d0), 1.0d0/sqrt(3.0d0)/)
+        wgp(1) = 1.0d0
+        wgp(2) = 1.0d0
+        wgp(3) = 1.0d0
+        wgp(4) = 1.0d0
+
+        !*********************************************************************!
+        ! Generate N and dN for all GP                                        !
+        !*********************************************************************!
+
+        allocate(N(nnode),dN(ndime,nnode))
+        allocate(Ngp(ngaus,nnode),dNgp(ndime,nnode,ngaus))
+
+        do igaus = 1,ngaus
+           s = xgp(igaus,1)
+           t = xgp(igaus,2)
+           N(1) = (1.0d0-s)*(1.0d0-t)
+           N(2) = (1.0d0+s)*(1.0d0-t)
+           N(3) = (1.0d0+s)*(1.0d0+t)
+           N(4) = (1.0d0-s)*(1.0d0+t)
+           N = 0.25d0*N
+           dN(1,1) = (-1.0d0+t)
+           dN(1,2) = ( 1.0d0-t)
+           dN(1,3) = ( 1.0d0+t)
+           dN(1,4) = (-1.0d0-t)
+           dN(2,1) = (-1.0d0+s)
+           dN(2,2) = (-1.0d0-s)
+           dN(2,3) = ( 1.0d0+s)
+           dN(2,4) = ( 1.0d0-s)
+           dN = 0.25d0*dN
+           Ngp(igaus,:) = N
+           dNgp(:,:,igaus) = dN
+        end do
+
+        !*********************************************************************!
+        ! Generate Jacobian related information                               !
+        !*********************************************************************!
+
+        allocate(elcod(ndime,nnode))
+        allocate(Je(ndime,ndime))
+        allocate(He(ndime,ndime))
+        allocate(struc_J(ndime,ndime,ngaus,nelem))
+        allocate(struc_H(ndime,ndime,ngaus,nelem))
+        allocate(struc_detJ(1,ngaus,nelem))
+        allocate(gpvol(1,ngaus,nelem))
+        allocate(dxN(ndime,nnode))
+        allocate(gpcar(ndime,nnode,ngaus,nelem))
+
+        do ielem = 1,nelem
+           do idime = 1,ndime
+              elcod(idime,1:nnode) = coord(connec(ielem,1:nnode),idime)
+           end do
+           do igaus = 1,ngaus
+              dN = dNgp(:,:,igaus)
+              Je = matmul(elcod,transpose(dN))        ! Jacobian at Gauss point
+              detJe = Je(1,1)*Je(2,2)-Je(2,1)*Je(1,2) ! Determinant of Jacobian at Gauss point
+              He(1,1) = Je(2,2)
+              He(2,2) = Je(1,1)
+              He(1,2) = -Je(1,2)
+              He(2,1) = -Je(2,1)
+              He = (1.0d0/detJe)*He
+              struc_J(:,:,igaus,ielem) = Je
+              struc_detJ(1,igaus,ielem) = detJe
+              struc_H(:,:,igaus,ielem) = He
+              dxN(1,:) = He(1,1)*dN(1,:)+He(1,2)*dN(2,:)
+              dxN(2,:) = He(2,1)*dN(1,:)+He(2,2)*dN(2,:)
+              gpcar(:,:,igaus,ielem) = dxN(:,:)
+              gpvol(1,igaus,ielem) = wgp(igaus)*detJe
+           end do
+        end do
+
+        !*********************************************************************!
+        ! Call convection subroutine                                          !
+        !*********************************************************************!
+
+        allocate(Rconvec(npoin,ndime))
+        call mom_convec(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u,q,pr,Rconvec)
+
+        do ipoin = 1,npoin
+           write(*,*) ipoin, Rconvec(ipoin,:)
+        end do
+
+        !*********************************************************************!
+        ! Compute Laplacian for comparison                                    !
+        !*********************************************************************!
+
+        if (abs(sum(Rconvec)) .gt. 0.00000000000001d0) then
+                write(*,*) "|sum(Rconvec)| = ", abs(sum(Rconvec))
+                stop 1
+        else
+                write(*,*) "|sum(Rconvec)| = ", abs(sum(Rconvec))
+                write(*,*) "TEST PASSED!"
+        end if
+
+end program unitt_mom_convec
---
+++ unitt/unitt_mom_convec/unitt_mom_convec.m
@@ -0,0 +1,102 @@
+clear;
+clc;
+
+pkg load symbolic
+
+syms s t
+
+% shape functions and gradN
+
+N = (1-s)*(1-t);
+N(2) = (1+s)*(1-t);
+N(3) = (1+s)*(1+t);
+N(4) = (1-s)*(1+t);
+N = (1/4)*N;
+
+dN = diff(N,s);
+dN(2,:) = diff(N,t);
+ndime = 2;
+
+% Mesh info
+
+conn = zeros(2,4);
+conn(1,:) = [1 2 5 6];
+conn(2,:) = [2 3 4 5];
+nelem = 2;
+nnode = 4;
+
+xy = zeros(6,2);
+xy(1,:) = [0.0 0.0];
+xy(2,:) = [2.0 0.0];
+xy(3,:) = [3.0 0.0];
+xy(4,:) = [3.5 3.0];
+xy(5,:) = [2.0 2.0];
+xy(6,:) = [0.0 2.0];
+npoin = 6;
+
+% Energy flux data
+
+u = zeros(6,2);
+q = zeros(6,2);
+pr = zeros(6,1);
+u(1,:) = [1.0 1.0];
+u(2,:) = [1.5 1.5];
+u(3,:) = [1.0 1.0];
+u(4,:) = [1.0 1.0];
+u(5,:) = [1.5 1.5];
+u(6,:) = [1.0 1.0];
+q = 1.2*u;
+pr(:,:) = 0.5;
+
+% Gaussian integ. table
+
+xgp = zeros(4,2);
+xgp(1,:) = [-1.0/sqrt(3.0) -1.0/sqrt(3.0)];
+xgp(2,:) = [1.0/sqrt(3.0) -1.0/sqrt(3.0)];
+xgp(3,:) = [1.0/sqrt(3.0) 1.0/sqrt(3.0)];
+xgp(4,:) = [-1.0/sqrt(3.0) 1.0/sqrt(3.0)];
+ngaus = 4;
+
+R = zeros(npoin,ndime);
+for ielem = 1:nelem
+  Re = zeros(nnode,ndime);
+  ind = conn(ielem,:);           % Nodal index
+  elq = q(ind,:);                % Element momentum
+  elu = u(ind,:);                % Element velocity
+  elpr = pr(ind,1);             % Element pressure
+  J = xy(ind,:)'*transpose(dN); % Element Jacobian
+  J = simplify(J);
+  detJ = simplify(det(J));      % Jacobian determinant
+  H = inv(J);                   % Jacobian inverse
+  dNcar = simplify(H*dN);       % Cartesian derivatives
+  aux = zeros(ndime,ndime);
+  %
+  % Compute symbolical divergence term
+  %
+  div = [s;s];
+  div(:,:) = 0;
+  for jnode = 1:nnode
+    for idime = 1:ndime
+      for jdime = 1:ndime
+        aux = elq(jnode,idime)*elu(jnode,jdime);
+        div(idime,1) = div(idime,1)+dNcar(jdime,jnode)*aux;
+      endfor
+    endfor
+  endfor
+  for igaus = 1:ngaus
+    divgp = double(subs(simplify(div),{s t},{xgp(igaus,1) xgp(igaus,2)}));
+    gpvol = double(subs(detJ,{s t},{xgp(igaus,1) xgp(igaus,2)}));
+    Ngp = double(subs(simplify(N),{s t},{xgp(igaus,1) xgp(igaus,2)}));
+    gpcar = double(subs(simplify(dNcar),{s t},{xgp(igaus,1) xgp(igaus,2)}));
+    for inode = 1:nnode
+      for idime = 1:ndime
+        aux = 0;
+        for jnode = 1:nnode
+          aux = aux+gpcar(idime,jnode)*elpr(jnode,1);
+        endfor
+        Re(inode,idime) = Re(inode,idime) + gpvol*Ngp(inode)*(divgp(idime)+aux);
+      endfor
+    endfor
+  endfor
+  R(ind,1:ndime) = R(ind,1:ndime)+Re(:,1:ndime);
+endfor
