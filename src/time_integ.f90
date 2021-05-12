e7485d7f54f37c431e897ec644a5c504d9dbe3ad
LucasGasparino <lucas.gasparino@bsc.es>
2021-05-13 01:50:36 +0200

    Corrected bad calls to diffusion subroutines. Added march parameter for release build.

--- src/time_integ.f90
+++ src/time_integ.f90
@@ -173,7 +173,7 @@ module time_integ
 
                       call mass_convec(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,q_1,Rmass_2)
                       if (flag_predic == 0) then
-                         call mass_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,rho(:,pos),mu_e,Rdiff_scal)
+                         call mass_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,rho_1,mu_e,Rdiff_scal)
                          Rmass_2 = Rmass_2 + Rdiff_scal
                       end if
                       call lumped_solver_scal(npoin,Ml,Rmass_2)
@@ -182,7 +182,7 @@ module time_integ
 
                       call mom_convec(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u_1,q_1,pr_1,Rmom_2)
                       if (flag_predic == 0) then
-                         call mom_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u(:,:,pos),mu_e,Rdiff_vect)
+                         call mom_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u_1,mu_e,Rdiff_vect)
                          Rmom_2 = Rmom_2 + Rdiff_vect
                       end if
                       call lumped_solver_vect(npoin,ndime,Ml,Rmom_2)
@@ -195,7 +195,7 @@ module time_integ
 
                       call ener_convec(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u_1,pr_1,E_1,Rener_2)
                       if (flag_predic == 0) then
-                         call ener_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u(:,:,pos),Tem(:,pos),mu_e,Rdiff_scal)
+                         call ener_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u_1,Tem_1,mu_e,Rdiff_scal)
                          Rener_2 = Rener_2 + Rdiff_scal
                       end if
                       call lumped_solver_scal(npoin,Ml,Rener_2)
@@ -244,7 +244,7 @@ module time_integ
 
                       call mass_convec(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,q_2,Rmass_3)
                       if (flag_predic == 0) then
-                         call mass_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,rho(:,pos),mu_e,Rdiff_scal)
+                         call mass_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,rho_2,mu_e,Rdiff_scal)
                          Rmass_3 = Rmass_3 + Rdiff_scal
                       end if
                       call lumped_solver_scal(npoin,Ml,Rmass_3)
@@ -253,7 +253,7 @@ module time_integ
 
                       call mom_convec(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u_2,q_2,pr_2,Rmom_3)
                       if (flag_predic == 0) then
-                         call mom_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u(:,:,pos),mu_e,Rdiff_vect)
+                         call mom_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u_2,mu_e,Rdiff_vect)
                          Rmom_3 = Rmom_3 + Rdiff_vect
                       end if
                       call lumped_solver_vect(npoin,ndime,Ml,Rmom_3)
@@ -266,7 +266,7 @@ module time_integ
 
                       call ener_convec(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u_2,pr_2,E_2,Rener_3)
                       if (flag_predic == 0) then
-                         call ener_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u(:,:,pos),Tem(:,pos),mu_e,Rdiff_scal)
+                         call ener_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u_2,Tem_2,mu_e,Rdiff_scal)
                          Rener_3 = Rener_3 + Rdiff_scal
                       end if
                       call lumped_solver_scal(npoin,Ml,Rener_3)
@@ -315,7 +315,7 @@ module time_integ
 
                       call mass_convec(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,q_3,Rmass_4)
                       if (flag_predic == 0) then
-                         call mass_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,rho(:,pos),mu_e,Rdiff_scal)
+                         call mass_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,rho_3,mu_e,Rdiff_scal)
                          Rmass_4 = Rmass_4 + Rdiff_scal
                       end if
                       call lumped_solver_scal(npoin,Ml,Rmass_4)
@@ -325,10 +325,8 @@ module time_integ
 
                       call mom_convec(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u_3,q_3,pr_3,Rmom_4)
                       if (flag_predic == 0) then
-                         call mom_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u(:,:,pos),mu_e,Rdiff_vect)
+                         call mom_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u_3,mu_e,Rdiff_vect)
                          Rmom_4 = Rmom_4 + Rdiff_vect
-                         call ener_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u(:,:,pos),Tem(:,pos),mu_e,Rdiff_scal)
-                         Rener_4 = Rener_4 + Rdiff_scal
                       end if
                       call lumped_solver_vect(npoin,ndime,Ml,Rmom_4)
                       call approx_inverse_vect(ndime,npoin,nzdom,rdom,cdom,ppow,Ml,Mc,Rmom_4)
@@ -341,7 +339,7 @@ module time_integ
 
                       call ener_convec(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u_3,pr_3,E_3,Rener_4)
                       if (flag_predic == 0) then
-                         call ener_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u(:,:,pos),Tem(:,pos),mu_e,Rdiff_scal)
+                         call ener_diffusion(nelem,ngaus,npoin,nnode,ndime,connec,Ngp,gpcar,gpvol,u_3,Tem_3,mu_e,Rdiff_scal)
                          Rener_4 = Rener_4 + Rdiff_scal
                       end if
                       call lumped_solver_scal(npoin,Ml,Rener_4)
