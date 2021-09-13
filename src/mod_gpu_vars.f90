09c85827b367f3d9d6ec83fdd5ed019d3b447c7b
Lucas Gasparino <lucas.gasparino@bsc.es>
2021-09-13 11:12:21 +0200

    Module with gpu variables.

---
+++ src/mod_gpu_vars.f90
@@ -0,0 +1,33 @@
+module mod_gpu_vars
+
+   implicit none
+
+   ! Mesh information
+
+   integer(4), device, allocatable :: connec_d(:,:)
+   integer(4), device, allocatable :: lbnodes_d(:)
+
+   ! Primary variables
+
+   real(8), device, allocatable    :: mu_e_d(:)
+   real(8), device, allocatable    :: rho_d(:,:)
+   real(8), device, allocatable    :: pr_d(:,:)
+   real(8), device, allocatable    :: E_d(:,:)
+   real(8), device, allocatable    :: Tem_d(:,:)
+   real(8), device, allocatable    :: e_int_d(:,:)
+   real(8), device, allocatable    :: u_d(:,:,:)
+   real(8), device, allocatable    :: q_d(:,:,:)
+
+   ! Mass matrices
+
+   real(8), device, allocatable    :: Ml_d(:)
+   real(8), device, allocatable    :: Mc_d(:)
+
+   ! Elemental info
+
+   real(8), device, allocatable    :: gpvol_d(:,:,:)
+   real(8), device, allocatable    :: gpcar_d(:,:,:,:)
+   real(8), device, allocatable    :: Ngp_d(:,:)
+
+
+end module mod_gpu_vars
