26e47133d7b706a401956038179f063a1b306b69
Lucas Gasparino <lucas.gasparino@bsc.es>
2021-09-28 01:53:23 +0200

    Cleaned up *.mod files in unitt_HEX27, added quadrature test. Added Stop criteria to tests.

--- unitt/unitt_HEX27_test/unitt_hex27_test.f90
+++ unitt/unitt_HEX27_test/unitt_hex27_test.f90
@@ -16,11 +16,13 @@ program unitt_hex27_test
    integer(4)           :: jdime, jnode, jgaus
    real(8)              :: s, t, z
    real(8)              :: detJe
-   real(8)              :: sumNa, sumsumdNa
-   real(8)              :: aux01
+   real(8)              :: sumNa, sumsumdNa, sumWgp
+   real(8)              :: gpvol
+   real(8)              :: aux01, eps
    real(8), allocatable :: elcod(:,:)
    real(8), allocatable :: N(:), dN(:,:)
    real(8), allocatable :: Je(:,:), He(:,:)
+   real(8), allocatable :: xgp(:,:), wgp(:)
    logical              :: flag ! Test fail indicator
 
    !
@@ -35,6 +37,7 @@ program unitt_hex27_test
 
    ndime = 3                    ! Element dimensions
    nnode = 27                   ! Nodes on element
+   ngaus = 27                   ! 4th order quadrature (check order)
 
    allocate(elcod(ndime,nnode)) ! Nodal coordinates array
 
@@ -88,10 +91,12 @@ program unitt_hex27_test
          call hex27(s,t,z,N,dN)
          ! Test
          if (jnode==inode .and. N(inode) .ne. 1.0d0) then ! Na(xa) = 1
+            write(*,*) 'ERROR: Shape function not equal to 1 at corresponding node!'
             write(*,*) inode, jnode, N(inode)
             flag = .false.
             exit
          else if (jnode .ne. inode .and. abs(N(inode)) .ne. 0.0d0) then ! Na(xb) = 0
+            write(*,*) 'ERROR: Shape function not equal to 0 at non-corresponding node!'
             write(*,*) inode, jnode, N(inode)
             flag = .false.
             exit
@@ -102,6 +107,11 @@ program unitt_hex27_test
          exit
       end if
    end do
+
+   if (flag .eqv. .false.) then
+      write(*,*) 'ERROR: Shape function failure!'
+      stop 1
+   end if
 
    !
    ! Test sum(Na(xgp)) = 1 and sum(sum(dNa(xgp))) = 0
@@ -118,12 +128,19 @@ program unitt_hex27_test
 
    ! Test
    if (sumNa .ne. 1.0d0) then
+      write(*,*) 'ERROR: Shape function failure!'
       write(*,*) sumNa
       flag = .false.
    end if
    if (abs(sumsumdNa) .ne. 0.0d0) then
+      write(*,*) 'ERROR: Shape function derivative failure!'
       write(*,*) sumsumdNa
       flag = .false.
+   end if
+
+   if (flag .eqv. .false.) then
+      write(*,*) 'ERROR: Sum shape function failure!'
+      stop 1
    end if
 
    !
@@ -139,5 +156,66 @@ program unitt_hex27_test
    z = 0.00d0                                           ! X3 gp of order 1
    call hex27(s,t,z,N,dN)                               ! Fill N and dN at Gauss point
    call elem_jacobian(ndime,nnode,elcod,dN,Je,detJe,He) ! Compute Je, detJe and inv(Je)
+   ! Test
+   do idime = 1,ndime
+      do jdime = 1,ndime
+         if (jdime == idime) then
+            if (Je(idime,jdime) .ne. 1.0d0) then
+               write(*,*) 'Error on Jacobian entry ', idime,idime
+               flag = .false.
+               exit
+            end if
+         else
+            if (Je(idime,jdime) .ne. 0.0d0) then
+               write(*,*) 'Error on Jacobian entry ', idime,jdime
+               flag = .false.
+               exit
+            end if
+         end if
+      end do
+      if (flag .eqv. .false.) then
+         exit
+      end if
+   end do
+
+   if (flag .eqv. .false.) then
+      write(*,*) 'ERROR: Malformed Jacobian!'
+      stop 1
+   end if
+
+   !
+   ! Test quadrature with 27 nodes
+   !
+
+   allocate(wgp(ngaus))              ! Allocate Gauss nodes weights
+   allocate(xgp(ngaus,ndime))        ! Allocate Gauss nodes coordinates
+   call gll_hex(ndime,ngaus,xgp,wgp) ! Create Gaussian quadrature table
+
+   gpvol = 0.0d0               ! Set computed volume to 0
+   sumWgp = 0.0d0              ! Initialize sum of wgp
+   eps = 0.00000000001d0       ! Set tolerance
+   do igaus = 1,ngaus
+      s = xgp(igaus,1)
+      t = xgp(igaus,2)
+      z = xgp(igaus,3)
+      call hex27(s,t,z,N,dN)                               ! Fill N and dN at Gauss points
+      call elem_jacobian(ndime,nnode,elcod,dN,Je,detJe,He) ! Compute Je, detJe and inv(Je)
+      gpvol = gpvol + wgp(igaus)*detJe
+      sumWgp = sumWgp + wgp(igaus)
+   end do
+   ! Test
+   if (gpvol .lt. 8.0d0-eps .or. gpvol .gt. 8.0d0+eps) then
+      write(*,*) 'GPVOL = ', gpvol
+      flag = .false.
+   end if
+   if (sumWgp .lt. 8.0d0-eps .or. sumWgp .gt. 8.0d0+eps) then
+      write(*,*) 'sumWgp = ', sumWgp
+      flag = .false.
+   end if
+
+   if (flag .eqv. .false.) then
+      write(*,*) 'ERROR: Bad Gaussian integration!'
+      stop 1
+   end if
 
 end program unitt_hex27_test
